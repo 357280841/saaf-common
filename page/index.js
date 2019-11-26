f68fdd8653ced8aab5ca676df71c920f516f5db3
郑晓文 <357280841@qq.com>
2019-11-26 15:08:32 +0800

    增加组件导出口子

---
+++ page/index.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: zhengxiaowen; 357280841@qq.com; 
+ * @Date: 2019-11-26 15:02:42 
+ * @Last Modified by: zhengxiaowen
+ * @Last Modified time: 2019-11-26 15:04:37
+ */
+
+import Home from './home/Home'
+import Iframe from './iframe/Iframe'
+import IframeBox from './iframe/IframeBox'
+import Login from './login/Login'
+import Main from './main/Main'
+
+export {
+    Home,
+    Iframe,
+    IframeBox,
+    Login,
+    Main,
+}
