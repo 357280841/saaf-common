9b38132503cf7629499fd88f966353268b8cc228
彭健强 <18820121479@163.com>
2019-12-02 16:15:53 +0800

    no message

--- demo/ImportAnExportDemo.vue
+++ demo/ImportAnExportDemo.vue
@@ -11,18 +11,54 @@
     <div>
         <Button @click="click">导入</Button>
         <SaafImportExcel v-model="show" :templetId="139"></SaafImportExcel>
+        <div>
+            <ImportExcel :header-config="headerConfig"/>
+        </div>
     </div>
 </template>
 <script>
-
+import ImportExcel from '../components/Excel/ImportExcel'
 export default {
     props:{
     },
     components: {
+        ImportExcel
     },
     data () {
         return {
-            show: false
+            show: false,
+            headerConfig: {
+                "sheet1": [
+                    {
+                        name: '育婴顾问姓名',
+                        require: true
+                    },
+                    {
+                        name: '是否发薪',
+                        require: true
+                    },
+                    {
+                        name: '是否仅报销量'
+                    }
+                ],
+                "sheet2": [
+                    {
+                        name: '入职日期',
+                        require: true
+                    },
+                    {
+                        name: '性别'
+                    },
+                    {
+                        name: '现居住地址',
+                        require: true
+                    },
+                    {
+                        name: '身份证号码',
+                        require: true
+                    }
+                ],
+            }
         }
     },
     mounted () {
