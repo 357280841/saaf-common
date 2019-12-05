2b9a73549cb14e0e2b23729a544b7f079ef8aac5
郑晓文 <357280841@qq.com>
2019-12-05 09:51:57 +0800

    增加404页面

---
+++ page/error/Error404.vue
@@ -0,0 +1,38 @@
+/*
+ * @Author: zhengxiaowen; 357280841@qq.com; 
+ * @Date: 2019-12-05 09:45:15 
+ * @Last Modified by: zhengxiaowen
+ * @Last Modified time: 2019-12-05 09:50:04
+ */
+
+
+<template>
+  <div class="p10">
+      <h2>404，没找到该页面</h2>
+  </div>
+</template>
+
+<script>
+import { tabsTool, getUrl } from "saaf-common";
+export default {
+    data() {
+      return {
+      }
+    },
+    computed: {
+
+    },
+    created() {
+    },
+    mounted(){
+      
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- page/index.js
+++ page/index.js
@@ -2,7 +2,7 @@
  * @Author: zhengxiaowen; 357280841@qq.com; 
  * @Date: 2019-11-26 15:02:42 
  * @Last Modified by: zhengxiaowen
- * @Last Modified time: 2019-11-26 15:04:37
+ * @Last Modified time: 2019-12-05 09:47:20
  */
 
 import Home from './home/Home'
@@ -10,6 +10,7 @@ import Iframe from './iframe/Iframe'
 import IframeBox from './iframe/IframeBox'
 import Login from './login/Login'
 import Main from './main/Main'
+import Error404 from './error/Error404'
 
 export {
     Home,
@@ -17,4 +18,5 @@ export {
     IframeBox,
     Login,
     Main,
+    Error404,
 }
